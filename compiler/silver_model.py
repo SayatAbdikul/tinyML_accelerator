0e74a2c3faad3329e3c09d96488e6e2a7a6cf699
Sayat <127617769+SayatAbdikul@users.noreply.github.com>
2025-07-14 21:26:40 +0500

    created an emulator for hardware
    It is done to check with the actual inference

---
+++ compiler/silver_model.py
@@ -0,0 +1,109 @@
+import os
+import numpy as np
+from dram import read_from_dram
+from helper_functions import quantize_int32_to_int8
+buffers = {}
+
+def load_memory(dram_file):
+    """Load memory from a hex file."""
+    with open(dram_file, 'r') as file:
+        memory = [np.int8(int(line.strip(), 16)) for line in file]
+    if not np.array_equal(memory, read_from_dram(0, len(memory))):
+        raise ValueError("Warning: Memory loaded from file does not match DRAM content.")
+    return memory
+
+
+def i_decoder(instruction):
+    opcode = instruction & 0x1F  # last 5 bits
+    
+    if opcode == 1: # LOAD_V
+        dest = instruction >> 5 & 0x1F
+        len = instruction >> 10 & 0x3FFFF # next 18 bits
+        addr = instruction >> 40 & 0xFFFFFF # next 24 bits 
+        load_v(dest, addr, len)
+        # print(f"LOAD_V: dest={dest}, addr={addr:#010x}, len={len}")
+
+    elif opcode == 2: # LOAD_M
+        dest = instruction >> 5 & 0x1F
+        cols = instruction >> 10 & 0x3FF # next 10 bits
+        rows = instruction >> 20 & 0x3FF # next 10 bits
+        addr = instruction >> 40 & 0xFFFFFF # next 24 bits
+        load_m(dest, addr, rows, cols)
+
+    elif opcode == 3: # STORE
+        buf_id = instruction >> 5 & 0x1F
+        len = instruction >> 10 & 0x3FFFF # next 18 bits
+        addr = instruction >> 40 & 0xFFFFFF # next 24 bits
+        store(buf_id, addr, len)
+
+    elif opcode == 4: # GEMV
+        dest = instruction >> 5 & 0x1F
+        cols = instruction >> 10 & 0x3FF
+        rows = instruction >> 20 & 0x3FF
+        b = instruction >> 30 & 0x1F
+        x = instruction >> 35 & 0x1F
+        w = instruction >> 40 & 0x1F
+        gemv(dest, w, x, b, rows, cols)
+    elif opcode == 5: # RELU
+        dest = instruction >> 5 & 0x1F
+        x = instruction >> 10 & 0x1F
+        relu(dest, x)
+    else:
+        return f"UNKNOWN_OPCODE: {opcode}"
+
+def load_v(dest, addr, length):
+    """Load vector from memory to buffer."""
+    # if addr == 0x10000:  # Print the input
+    #     print(f"LOAD_V on buffer {dest}: the array is {memory[addr:addr + length]} at address {addr:#010x} with length {length}")
+    buffers[dest] = memory[addr:addr + length]  # Load vector from memory to buffer
+
+def load_m(dest, addr, rows, cols):
+    buffers[dest] = buffers[dest] = memory[addr:addr + rows * cols]  
+    # if addr == 0x20000:  # Print the weights
+    #     print(f"LOAD_M on buffer {dest}: the matrix is {buffers[dest]} at address {addr:#010x} with rows={rows}, cols={cols}")
+    # print
+    # print("The number of unique values in the output buffer of matrix is:", np.unique(buffers[dest]).size)
+
+
+def store(buf_id, addr, length):
+    """Store buffer to memory."""
+    quantized_output_scale = 0.1
+    quantized_output_zero_point = 0
+    buffers[buf_id] = quantize_int32_to_int8(np.array(buffers[buf_id], dtype=np.int32), quantized_output_scale, quantized_output_zero_point)
+    for i in range(length):
+        memory[addr + i] = buffers[buf_id][i]  # Extract byte from buffer
+
+flag = 0
+def gemv(dest, w, x, b, rows, cols):
+    """Perform GEMV operation."""
+    global flag
+    buffers[dest] = [0] * rows  # Initialize destination buffer with zeros
+    # print(f"GEMV: dest={dest}, w={w}, x={x}, b={b}, rows={rows}, cols={cols}")
+    for i in range(rows):
+        sum = 0
+        for j in range(cols):
+            sum += buffers[w][i * cols + j] * buffers[x][j] # Matrix-vector multiplication
+            # if flag == 0:  # Print only once
+                # print(f"Multiplying w[{i * cols + j}]={buffers[w][i * cols + j]} with x[{j}]={buffers[x][j]} to sum={sum}")
+
+        sum += buffers[b][i]
+        buffers[dest][i] = np.int32(sum)  # Store the result in the destination buffer
+    # print("The number of unique values in the output buffer is:", np.unique(buffers[dest]).size)
+    # print("The output buffer is:", buffers[dest])
+    flag = 1  # Set flag to indicate GEMV has been executed
+
+def relu(dest, x):
+    """Apply ReLU activation."""
+    buffers[dest] = [max(0, val) for val in buffers[x]]  # Apply ReLU to each element in the buffer
+
+def execute_program(hex_file):
+    """Execute the program from a hex file."""
+    with open(hex_file, 'r') as file:
+        instructions = [int(line.strip(), 16) for line in file if line.strip()]
+    global memory
+    memory = load_memory('dram.hex')
+    for instruction in instructions:
+        i_decoder(instruction)
+
+    return buffers[6]  # Return the final output buffer (assuming it's always buffer 6)
+    
