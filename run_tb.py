d29dbdcb4dfc583f66c9959779a881cfcc8daefc
Sayat <127617769+SayatAbdikul@users.noreply.github.com>
2025-09-05 12:21:54 +0500

    removed the missing module

--- run_tb.py
+++ run_tb.py
@@ -7,21 +7,20 @@ print("3. Scale Calculator Test")
 print("4. Quantizer Pipeline Test")
 print("5. Quantizer Overall Test")
 print("6. Wallace Multiplier Test")
-print("7. GEMV Buffer File Test")
-print("8. PE Test")
-print("9. Memory Test")
-print("10. Buffer File Test")
-print("11. Load Matrix Test")
-print("12. Top GEMV Test")
-print("13. ReLU Test")
-print("14. Load Vector Test")
-print("15. Fetch Unit Test")
-print("16. tinyML Accelerator Top Test")
-print("17. Integrated Top Module Test")
-print("18. Execution Unit Test")
+print("7. PE Test")
+print("8. Memory Test")
+print("9. Buffer File Test")
+print("10. Load Matrix Test")
+print("11. Top GEMV Test")
+print("12. ReLU Test")
+print("13. Load Vector Test")
+print("14. Fetch Unit Test")
+print("15. tinyML Accelerator Top Test")
+print("16. Integrated Top Module Test")
+print("17. Execution Unit Test")
 
-choice = input("Enter your choice (1-18): ")
-if choice not in {'1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18'}:
+choice = input("Enter your choice (1-17): ")
+if choice not in {'1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17'}:
     print("Invalid choice. Exiting.")
     exit(1)
 # Run the selected test based on user input
@@ -36,7 +35,7 @@ commands = {
     '3': ["verilator -Wall --cc rtl/scale_calculator.sv --exe test/scale_calculator_tb.cpp",
           "make -C obj_dir -f Vscale_calculator.mk Vscale_calculator",
           "./obj_dir/Vscale_calculator"],
-    '4': ["verilator -Wall --cc rtl/quantizer_pipeline.sv --exe test/quantizer_pipeline_tb.cpp",
+    '4': ["verilator -Wall --cc rtl/quantizer_pipeline.sv --exe rtl/compressor_3to2.sv rtl/wallace_32x32.sv test/quantizer_pipeline_tb.cpp",
           "make -C obj_dir -f Vquantizer_pipeline.mk Vquantizer_pipeline",
           "./obj_dir/Vquantizer_pipeline"],
     '5': ["verilator -Wall --cc rtl/scale_calculator.sv rtl/wallace_32x32.sv rtl/compressor_3to2.sv rtl/quantizer_pipeline.sv rtl/quantization.sv --top quantization --exe test/quantization_tb.cpp",
@@ -45,41 +44,38 @@ commands = {
       '6': ["verilator -Wall --cc rtl/wallace_32x32.sv rtl/compressor_3to2.sv --top wallace_32x32 --exe test/wallace_32x32_tb.cpp --trace",
           "make -C obj_dir -f Vwallace_32x32.mk Vwallace_32x32",
           "./obj_dir/Vwallace_32x32"],
-      '7': ["verilator -Wall --cc rtl/gemv_buffer_file.sv --exe test/gemv_buffer_file_tb.cpp",
-          "make -C obj_dir -f Vgemv_buffer_file.mk Vgemv_buffer_file",
-          "./obj_dir/Vgemv_buffer_file"],
-      '8': ["verilator -Wall --cc rtl/pe.sv --exe test/pe_tb.cpp",
+      '7': ["verilator -Wall --cc rtl/pe.sv --exe test/pe_tb.cpp",
           "make -C obj_dir -f Vpe.mk Vpe",
           "./obj_dir/Vpe"],
-      '9': ["verilator -Wall --cc rtl/simple_memory.sv --exe test/simple_memory_tb.cpp",
+      '8': ["verilator -Wall --cc rtl/simple_memory.sv --exe test/simple_memory_tb.cpp",
           "make -C obj_dir -f Vsimple_memory.mk Vsimple_memory",
           "./obj_dir/Vsimple_memory"],
-    '10': ["verilator -Wall --trace --cc rtl/buffer_file.sv --exe test/buffer_file_tb.cpp",
+    '9': ["verilator -Wall --trace --cc rtl/buffer_file.sv --exe test/buffer_file_tb.cpp",
             "make -C obj_dir -f Vbuffer_file.mk Vbuffer_file",
             "./obj_dir/Vbuffer_file"],
-    '11': ["verilator -Wall --cc rtl/load_m.sv rtl/simple_memory.sv --top load_m --exe test/load_m_tb.cpp",
+    '10': ["verilator -Wall --cc rtl/load_m.sv rtl/simple_memory.sv --top load_m --exe test/load_m_tb.cpp",
             "make -C obj_dir -f Vload_m.mk Vload_m",
             "./obj_dir/Vload_m"],
-    '12': ["verilator -Wall --trace --cc rtl/top_gemv.sv rtl/pe.sv rtl/scale_calculator.sv rtl/quantizer_pipeline.sv "
+    '11': ["verilator -Wall --trace --cc rtl/top_gemv.sv rtl/pe.sv rtl/scale_calculator.sv rtl/quantizer_pipeline.sv "
             "rtl/wallace_32x32.sv rtl/compressor_3to2.sv --top top_gemv --exe test/top_gemv_tb.cpp",
             "make -C obj_dir -f Vtop_gemv.mk Vtop_gemv",
             "./obj_dir/Vtop_gemv"],
-    '13': ["verilator -Wall --cc rtl/relu.sv --exe test/relu_tb.cpp",
+    '12': ["verilator -Wall --cc rtl/relu.sv --exe test/relu_tb.cpp",
             "make -C obj_dir -f Vrelu.mk Vrelu",
             "./obj_dir/Vrelu"],
-    '14': ["verilator -Wall --cc rtl/load_v.sv rtl/simple_memory.sv --top load_v --exe test/load_v_tb.cpp",
+    '13': ["verilator -Wall --cc rtl/load_v.sv rtl/simple_memory.sv --top load_v --exe test/load_v_tb.cpp",
             "make -C obj_dir -f Vload_v.mk Vload_v",
             "./obj_dir/Vload_v"],
-    '15': ["verilator -Wall --cc rtl/fetch_unit.sv rtl/simple_memory.sv --top fetch_unit --exe test/fetch_unit_tb.cpp",
+    '14': ["verilator -Wall --cc rtl/fetch_unit.sv rtl/simple_memory.sv --top fetch_unit --exe test/fetch_unit_tb.cpp",
             "make -C obj_dir -f Vfetch_unit.mk Vfetch_unit",
             "./obj_dir/Vfetch_unit"],
-    '16': ["verilator -Wall --trace --cc rtl/tinyml_accelerator_top.sv rtl/execution_unit.sv rtl/buffer_file.sv rtl/fetch_unit.sv rtl/i_decoder.sv rtl/load_m.sv rtl/load_v.sv rtl/top_gemv.sv rtl/pe.sv rtl/scale_calculator.sv rtl/quantizer_pipeline.sv rtl/wallace_32x32.sv rtl/compressor_3to2.sv rtl/relu.sv rtl/simple_memory.sv --top tinyml_accelerator_top --exe test/tinyml_accelerator_top_tb.cpp",
+    '15': ["verilator -Wall --trace --cc rtl/tinyml_accelerator_top.sv rtl/execution_unit.sv rtl/buffer_file.sv rtl/fetch_unit.sv rtl/i_decoder.sv rtl/load_m.sv rtl/load_v.sv rtl/top_gemv.sv rtl/pe.sv rtl/scale_calculator.sv rtl/quantizer_pipeline.sv rtl/wallace_32x32.sv rtl/compressor_3to2.sv rtl/relu.sv rtl/simple_memory.sv --top tinyml_accelerator_top --exe test/tinyml_accelerator_top_tb.cpp",
             "make -C obj_dir -f Vtinyml_accelerator_top.mk Vtinyml_accelerator_top",
             "./obj_dir/Vtinyml_accelerator_top"],
-    '17': ["verilator -Wall --trace --cc rtl/top.sv rtl/tinyml_accelerator_top.sv rtl/execution_unit.sv rtl/buffer_file.sv rtl/fetch_unit.sv rtl/i_decoder.sv rtl/load_m.sv rtl/load_v.sv rtl/top_gemv.sv rtl/pe.sv rtl/scale_calculator.sv rtl/quantizer_pipeline.sv rtl/wallace_32x32.sv rtl/compressor_3to2.sv rtl/relu.sv rtl/simple_memory.sv --top top --exe test/top_integrated_tb.cpp",
+    '16': ["verilator -Wall --trace --cc rtl/top.sv rtl/tinyml_accelerator_top.sv rtl/execution_unit.sv rtl/buffer_file.sv rtl/fetch_unit.sv rtl/i_decoder.sv rtl/load_m.sv rtl/load_v.sv rtl/top_gemv.sv rtl/pe.sv rtl/scale_calculator.sv rtl/quantizer_pipeline.sv rtl/wallace_32x32.sv rtl/compressor_3to2.sv rtl/relu.sv rtl/simple_memory.sv --top top --exe test/top_integrated_tb.cpp",
             "make -C obj_dir -f Vtop.mk Vtop",
             "./obj_dir/Vtop"],
-    '18': ["verilator -Wall --trace --cc rtl/execution_unit.sv rtl/simple_memory.sv rtl/buffer_file.sv rtl/top_gemv.sv rtl/pe.sv rtl/scale_calculator.sv rtl/quantizer_pipeline.sv "
+    '17': ["verilator -Wall --trace --cc rtl/execution_unit.sv rtl/simple_memory.sv rtl/buffer_file.sv rtl/top_gemv.sv rtl/pe.sv rtl/scale_calculator.sv rtl/quantizer_pipeline.sv "
         "rtl/wallace_32x32.sv rtl/load_v.sv rtl/load_m.sv rtl/compressor_3to2.sv rtl/relu.sv --top execution_unit --exe test/execution_unit_tb.cpp",
             "make -C obj_dir -f Vexecution_unit.mk Vexecution_unit",
             "./obj_dir/Vexecution_unit"],
